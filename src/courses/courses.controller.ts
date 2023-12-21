70697ce0647f8974a0d2f678cce8148461c34c38
Marwam Malta <marwamprog.js@gmail.com>
2023-12-21 14:44:05 -0300

    feat: Criando entidade de Tags

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,44 +1,51 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CoursesService } from './courses.service';
 import { CreateCourseDTO } from './dto/create-course.dto';
 import { UpdateCourseDTO } from './dto/update-course.dto';
 
 @Controller('courses')
 export class CoursesController {
-    
-    constructor(
-        private readonly courseService: CoursesService
-    ) {}
+  constructor(private readonly courseService: CoursesService) {}
 
-    @Get()
-    findAll() {
-        return this.courseService.findAll();
-    }
+  @Get()
+  findAll() {
+    return this.courseService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id') id: number) {
-        return this.courseService.findOne(id);
-    }
-    
-    @Post()
-    create(@Body() createCourseDTO: CreateCourseDTO) {
-        return this.courseService.create(createCourseDTO);
-    }
+  @Get(':id')
+  findOne(@Param('id') id: number) {
+    return this.courseService.findOne(id);
+  }
 
-    @Put(':id')
-    update(@Param('id') id: number, @Body() updateCourseDTO: UpdateCourseDTO) {
-        return this.courseService.update(id, updateCourseDTO);
-    }
+  @Post()
+  create(@Body() createCourseDTO: CreateCourseDTO) {
+    return this.courseService.create(createCourseDTO);
+  }
 
-    @HttpCode(HttpStatus.NO_CONTENT)
-    @Delete(':id')
-    remove(@Param('id') id: number) {
-        this.courseService.remove(id);
-    }
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updateCourseDTO: UpdateCourseDTO) {
+    return this.courseService.update(id, updateCourseDTO);
+  }
 
-    // @HttpCode(HttpStatus.NO_CONTENT)
-    // @Delete(':id')
-    // remove(@Param('id') id: number) {
-        
-    // }
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete(':id')
+  remove(@Param('id') id: number) {
+    this.courseService.remove(id);
+  }
+
+  // @HttpCode(HttpStatus.NO_CONTENT)
+  // @Delete(':id')
+  // remove(@Param('id') id: number) {
+
+  // }
 }
--- src/courses/entities/tags.entity.ts
+++ src/courses/entities/tags.entity.ts
@@ -1,16 +1,10 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@Entity('courses')
-export class Course {
+@Entity('tags')
+export class Tag {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column()
   name: string;
-
-  @Column()
-  description: string;
-
-  @Column('json', { nullable: true })
-  tags: string[];
 }
