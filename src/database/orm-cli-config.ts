d72894f9f93867ab884273de73f70122d58b9782
Marwam Malta <marwamprog.js@gmail.com>
2023-12-22 12:49:27 -0300

    feat: Refatorando modulo de databse e orm-cli-config.ts para utilizar as variaveis de ambiente .env

--- src/database/orm-cli-config.ts
+++ src/database/orm-cli-config.ts
@@ -1,10 +1,23 @@
-import { DataSource } from 'typeorm';
-import { dataSourceOptions } from './database.module';
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { CreateCoursesTable1703186600082 } from '../migrations/1703186600082-CreateCoursesTable';
 import { CreateTagsTable1703188175623 } from '../migrations/1703188175623-CreateTagsTable';
 import { CreateCoursesTagsTable1703244317449 } from '../migrations/1703244317449-CreateCoursesTagsTable';
 import { AddCoursesIdToCoursesTagsTable1703245021256 } from '../migrations/1703245021256-AddCoursesIdToCoursesTagsTable';
 import { AddTagsIdToCoursesTagsTable1703246089065 } from '../migrations/1703246089065-AddTagsIdToCoursesTagsTable';
+import { Course } from '../courses/entities/courses.entity';
+import { Tag } from '../courses/entities/tags.entity';
+
+export const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  entities: [Course, Tag],
+  synchronize: false,
+};
 
 export const dataSource = new DataSource({
   ...dataSourceOptions,
